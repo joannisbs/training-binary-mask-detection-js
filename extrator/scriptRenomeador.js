468f5c054832fba5df0aba777b4ea825964122db
joannis-lbca <joannis.stavrakas@lbca.com.br>
2021-07-15 13:23:15 -0300

    ADD: primeiro treino

--- extrator/scriptRenomeador.js
+++ extrator/scriptRenomeador.js
@@ -2,17 +2,19 @@ const { join } = require('path');
 const { readdirSync, renameSync } = require('fs');
 // const [dir, search, replace] = process.argv.slice(2);
 
-const dir = './photosMasked'
-const search = 'foto'
-const replace = 'foto-a_'
+const dir = '../with_mask'
+const search = '.+[\.]'
+const replace = '.'
 const match = RegExp(search, 'g');
 const files = readdirSync(dir);
 
+
+const count = 0
 files
-  .filter(file => file.match(match))
+  // .filter(file => file.match(match))
   .forEach(file => {
     const filePath = join(dir, file);
-    const newFilePath = join(dir, file.replace(match, replace));
-
+    const newFilePath = join(dir, file.replace(match, count + replace));
+    count ++;
     renameSync(filePath, newFilePath);
   });
