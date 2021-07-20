957989f89044696563429d0970b518d881fd3370
Matheus Coelho <matheus.coelho@aluno.ufabc.edu.br>
2021-07-19 21:02:27 -0300

    ADD: Método shuffle e random.

---
+++ src/utilitiesClass.js
@@ -0,0 +1,29 @@
+
+class Utilities {
+  shuffle(array) {
+    let index_i = array.length, index_j, index_k;
+
+    // While there remain elements to shuffle…
+    while (index_i) {
+
+      // Pick a remaining element…
+      index_k = Math.floor(Math.random() * index_i--);
+
+      // And swap it with the current element.
+      index_j = array[index_i];
+      array[index_i] = array[index_k];
+      array[index_k] = index_j;
+    }
+
+    return array;
+  }
+
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+}
+
+module.exports = Utilities;
