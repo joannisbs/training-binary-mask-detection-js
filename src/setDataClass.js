fcfb9a7fcc024906e79a226e74c8d79929416e63
Matheus Coelho <matheus.coelho@aluno.ufabc.edu.br>
2021-07-19 21:06:05 -0300

    ADD: Identificando diretórios e arquivos.

---
+++ src/setDataClass.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+class SettingData {
+  reader = undefined;
+  tags = [];
+  dataSet = [];
+	constructor(reader){
+    this.reader = reader;
+  }
+
+  setPathToTag(path, className){
+    this.tags.push({ path, class: className });
+  }
+
+  getDataSet(){
+    this.tags.forEach(tag => {
+      const fileNames = fs.readdirSync(tag.path);
+      fileNames.forEach(fileName => {
+        this.dataSet.push({
+          data: this.reader(tag.path, fileName),
+          class: tag.class,
+        });
+      });
+    })
+  }
+}
