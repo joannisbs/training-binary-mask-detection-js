a2ee120d5be8436da6e7e25217e55fdb91b6b89b
Matheus Coelho <matheus.coelho@aluno.ufabc.edu.br>
2021-07-28 20:15:03 -0300

    FIX: Tirando imports não utilizados.

--- src/makeAmountTrainTest.js
+++ src/makeAmountTrainTest.js
@@ -1,8 +1,4 @@
 const utilitiesClass = require("./utilitiesClass");
-const setDataClass = require("./setDataClass");
-const reaImageClass = require("./readImageClass");
-const readImage = new reaImageClass();
-const data = new setDataClass();
 const utilities = new utilitiesClass();
 
 function makeAmountTrainTest(dataSet, factor){
@@ -10,11 +6,8 @@ function makeAmountTrainTest(dataSet, factor){
   dataSet = utilities.shuffle(dataSet);
     
   const testFactor = Math.ceil(dataSet.length * factor);
-
   const traningArray = dataSet.slice(0, testFactor);
   const testArray = dataSet.slice(testFactor, dataSet.length);
 
   return { train: traningArray, test: testArray };
-
-
 }
